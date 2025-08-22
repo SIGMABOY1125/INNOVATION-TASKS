ee5f1e9d21f66204d494140386f4b9864e19a8d0
NISHANTH11 <niahanth111418@gmail.com>
2025-08-22 13:59:07 +0530

    Update task1.js

--- DAY 1/task1.js
+++ DAY 1/task1.js
@@ -1,8 +1,9 @@
 // --- Task 1: Basic JavaScript Program ---
-var name = "darsshini ragavan";
+var name = "nishanth";
 let age = 18;
 const city = "Coimbatore";
 
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("City:", city);
+
